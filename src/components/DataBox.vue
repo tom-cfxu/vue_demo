f296557b893a2d116d2b03690c284c79a5cd9174
tom <1157164295@qq.com>
2020-07-29 17:47:42 +0800

    123

---
+++ src/components/DataBox.vue
@@ -0,0 +1,54 @@
+<template class="template">
+  <div class="dataBox">
+    <i :class="icon"></i>
+    &nbsp;
+    {{name}} : {{value}}{{unit}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dataBox",
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    },
+    name: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    unit: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.dataBox {
+  // width: 100%;
+  min-height: 50px;
+  font-size: 20px;
+  text-align: left;
+  // border: 2px solid #36a383;
+  margin: 5px 10px;
+  padding-left: 2vw;
+  display: flex;
+  // justify-content: center;
+  font-family: "Helvetica";
+  align-items: center;
+  color: #fff;
+  border-radius: 10px;
+  background: linear-gradient(135deg, rgb(0, 238, 255), rgb(64, 141, 248));
+  box-shadow: -8px -8px 10px -12px rgb(196, 242, 248),
+    8px 8px 10px -5px rgba(0, 0, 0, 1);
+  user-select: none;
+}
+</style>
---
+++ src/views/Assemble/Assemble.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="assemble">
+    <BorderBox class="content">
+      <img class="img" src="../../assets/img/assemble.png" alt />
+    </BorderBox>
+  </div>
+</template>
+
+<script>
+import BorderBox from "../../components/BorderBox";
+export default {
+  name: "assemble",
+  components: {
+    BorderBox
+  }
+};
+</script>
+  
+<style lang="less">
+// @height: 650px;
+.assemble {
+  display: flex;
+  width: 100%;
+  height: 90%;
+  justify-content: center;
+  align-items: center;
+  .content {
+    // height: 650px;
+    position: relative;
+  }
+}
+.img {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  //   padding-top: 60px;
+}
+</style>
